b5b1f18c4e87e0f5a298bf2edbc56514ff43c449
Alex Howard <alexamh@zoho.com>
2022-05-18 17:41:21 -0400

    Primitive Types update

---
+++ Javascript/Intro to Javascript/Primitive Data Types/primitiveTypes.js
@@ -0,0 +1,20 @@
+/*
+Number
+String
+Boolean
+Null
+Undefined
+*/
+//Booleans - True/False
+alexIsSexy = true;
+alexIsLame = false;
+
+console.log(alexIsLame, alexIsSexy);
+
+//Variables are dynamically typed
+
+//Strings - characters wrapped in quotes ''  ""  `` - Backticks are for multiline comments
+//typeof - will tell what type is
+console.log(typeof 1);
+console.log(typeof 'Hi Bitch');
+console.log(typeof true);
