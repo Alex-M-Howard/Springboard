fd4094baf372884df78a26663373c573b58ebc06
Alex Howard <alexamh@zoho.com>
2022-05-19 19:29:18 -0400

    just because

--- Javascript/Intro to Javascript/Running Javascript in HTML/testing.js
+++ Javascript/Intro to Javascript/Running Javascript in HTML/testing.js
@@ -1,11 +1,11 @@
 let num = 5;
 
-for(let i = 0; i < num; i++){
-    console.log("Hello World")
+for (let i = 0; i < num; i++) {
+	console.log('Hello World');
 }
 
+console.log('WHAT?');
+//alert("It's working!");
+console.log('END');
 
-console.log("WHAT?")
-alert("It's working!")
-console.log("END")
-
+console.log('hi');
