87c959a83234df84eb660534a455f16b88c0e04b
Alex Howard <alexamh@zoho.com>
2022-06-12 01:00:59 -0400

    Started SpringBoard Unit 3 - WebFundamentals - 3.2 JavaScript Debugging

---
+++ Unit 3 - Web Fundamentals/3.2 - JavaScript Debugging/notes.js
@@ -0,0 +1,18 @@
+//Different errors
+/*
+syntax error - something is typed incorrectly
+reference error - variable is being referenced that doesn't exist, or isn't in same scope
+type error- Trying to do something with a variable that is not possible of a certain type. String method on an int for example
+*/
+
+
+//Anatomy of an error message
+/*
+
+Uncaught SyntaxError: Unexpected app.js:6 token ';'
+
+You can see the type of error, the file name, line number, and reason
+*/
+
+//Use console.log to help you debug when you get a bug that doesn't throw an error
+
