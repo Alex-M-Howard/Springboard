e83f4b8294802ac1a640a064868cab4ff4b94b1b
Alex Howard <alexamh@zoho.com>
2022-06-14 16:35:46 -0400

    Finished watching videos for Unit 4.2

--- master_notes.js
+++ master_notes.js
@@ -465,4 +465,75 @@ input.setAttribute('type', 'input');
 input2.value = '5'
 
 
+/////// ADDING NEW ELEMENTS TO HTML /////////
+// Make a new empty LI
+const newTodo = document.createElement('li');
+// Make a new empty B element
+const boldText = document.createElement('b');
+//Add some text to the new B element:
+boldText.textContent = "DON'T FORGET TO LOCK THE COOP!"
+//Add a class to
+newTodo.classList.add('todo');
+//Add the B element as a child of the LI
+newTodo.append(boldText);
+
+//Create a second LI
+const secondTodo = document.createElement('li');
+secondTodo.textContent = "Order more la croix";
+secondTodo.className = 'todo';
+
+// Select the UL we want to append to
+const ul = document.querySelector('ul');
+//Append both new LIs at once!
+ul.append(newTodo, secondTodo);
+
+//Prepending a new LI to the UL
+const thirdTodo = document.createElement('li');
+thirdTodo.textContent = "Feed Cats";
+ul.prepend(thirdTodo);
+
+// Making a new image
+const newImg = document.createElement('img');
+newImg.classList.add('thumbnail') //adding a class (to resize the img)
+// Setting the img src
+newImg.setAttribute('src', 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1320&q=80')
+
+document.body.prepend(newImg)
+
+
+
+//////// REMOVING ELEMENTS FROM HTML /////////
+
+OLD WAY -> remove CHILD by calling the parent, pass in child
+const removeMe = document.querySelector('#remove-me');
+ul.removeChild(removeMe) -> the unordered list is the parent
+
+
+NEW WAY -> remove()
+const removeMe = document.querySelector('#remove-me');
+removeMe.remove()
+
+
+
+
+////// PARENT CHILD SIBLINGS ///////
+Accessing a parent element: parentElement methods
+const div = document.querySelector('div')
+div.parentElement
+
+Accessing CHILDREN CHILD ELEMENT 
+div.children ->array like object
+div.firstElementChild
+div.lastElementChild
+
+ACCESSING SIBLINGS
+nextElementSibling - > If there isn't one, it will return null
+previousElementSibling
+
+
+///////// NODES vs ELEMENTS ////////
+NODES are everything in the DOM, including comments
+Every element is a node, but not all nodes are elements!
+.childNodes will show all underlying inside of an element
+
 */
