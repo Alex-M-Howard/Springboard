14b4e7be4190aa0ad5b61a8e24ed999464d66893
Alex Howard <alexamh@zoho.com>
2022-06-18 18:36:33 -0400

    Followed Springboard solution since mine was too complicated and didn't work

--- Unit 4 - DOM Manipulation/4.5 - Memory Game Challenge/script.js
+++ Unit 4 - DOM Manipulation/4.5 - Memory Game Challenge/script.js
@@ -1,152 +1,117 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const gameContainer = document.getElementById("game");
-  let guesses = 0;
-  const guessedCards = [];
-  let noClicking = false
-    
-  function sleep(milliseconds) {   //I'm having issues with setInterval and setTimeout being asynchronous
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
+const gameContainer = document.getElementById("game");
+
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
   }
 
-  const CARDS = [
-    "red",
-    "blue",
-    "green",
-    "orange",
-    "purple",
-    "red",
-    "blue",
-    "green",
-    "orange",
-    "purple"
-  ];
+  return array;
+}
 
-  // here is a helper function to shuffle an array
-  // it returns the same array with values shuffled
-  // it is based on an algorithm called Fisher Yates if you want ot research more
-  function shuffle(array) {
-    let counter = array.length;
+let shuffledColors = shuffle(COLORS);
 
-    // While there are elements in the array
-    while (counter > 0) {
-      // Pick a random index
-      let index = Math.floor(Math.random() * counter);
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
 
-      // Decrease counter by 1
-      counter--;
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
 
-      // And swap the last element with it
-      let temp = array[counter];
-      array[counter] = array[index];
-      array[index] = temp;
-    }
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
 
-    return array;
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
   }
+}
 
-  let shuffledColors = shuffle(CARDS);
+// TODO: Implement this function!
+function handleCardClick(event) {
+  // you can use event.target to see which element was clicked
+  console.log("you just clicked", event.target);
+  if (noClicking) return;
+  if (event.target.classList.contains("flipped")) return;
 
-  // this function loops over the array of colors
-  // it creates a new div and gives it a class with the value of the color
-  function createDivsForColors(colorArray) {
-    for (let color of colorArray) {
-      // create a new div
-      const newDiv = document.createElement("div");
+  let currentCard = event.target;
+  currentCard.style.backgroundColor = currentCard.classList[0];
 
-      // give it a class attribute for the value we are looping over
-      newDiv.classList.add(color);
-
-      // call a function handleCardClick when a div is clicked on
-      // newDiv.addEventListener("click", handleCardClick);
-
-      // append the div to the element with an id of game
-      gameContainer.append(newDiv);
+  if (!card1 || !card2) {
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    if (currentCard === card1) {
+      card2 = null;
+    } else {
+      card2 = currentCard;
     }
-  }
 
-
-    
-  const section = document.querySelector("section")
-  section.addEventListener("click", handleCardClick);
-    
-
-  function handleCardClick(event) {
-    if (event.target.id !== 'game' && !event.target.classList.value.includes("selected") && !noClicking) {
-      const color = event.target.classList.value;
-      const card = event.target
-      event.target.style.backgroundColor = color;
-      checkCards(card);
-    }
-  }
-
-
-  function checkCards(card) {
-    
-    guessedCards.push(card);
-    for (let card in guessedCards) {
-      console.log(guessedCards[card].style.backgroundColor)
-    }
-    card.classList.add('selected');
+    if (card1 && card2) {
+      noClicking = true;
       
-    if (guesses > 0) {
-      noClicking = true
-      if (guessedCards[0].classList.value === guessedCards[1].classList.value) {
-        for (let card in guessedCards) {
-          guessedCards[card].classList.add('matched');
-        }  
-
-        const match = document.getElementsByClassName("match hidden")[0];
-        match.classList.remove("hidden");
-        const timer = setTimeout(function () {
-          match.classList.add("hidden");
-        }, 1000);
-    
-        
-        } else {
-       
-        const timer = setTimeout(function () {
-          const selected = document.querySelectorAll(".selected");
-          for (card in selected) {
-            if (selected[card].classList.value.includes('matched')) {
-              continue;
-            }else{
-              selected[card].classList.remove('selected');
-              selected[card].style.backgroundColor = 'white';
-              console.log('works')
-            }
-          }
-  
-        }, 1000);
-          
-        }
-
-        noClicking = false
-        guessedCards.pop()
-        guessedCards.pop()
-        guesses = 0;
+      if (card1.className === card2.className) {
+        cardsFlipped += 2;
+        card1.removeEventListener("click", handleCardClick);
+        card2.removeEventListener("click", handleCardClick);
+        card1 = null;
+        card2 = null;
+        noClicking = false;
       } else {
-        guesses++;
+       const timer = setTimeout(function () {
+          card1.style.backgroundColor = 'white';
+          card2.style.backgroundColor = 'white';
+          card1.classList.remove("flipped");
+          card2.classList.remove("flipped");
+          card1 = null;
+          card2 = null;
+          noClicking = false;
+        }, 1000)
     }
-    
-    }
-    
-    
-    
-    
-    
+  }
 
+  }
+  if (cardsFlipped === COLORS.length) {
+    alert('GAME OVER')
+  }
 
+}
 
+// when the DOM loads
+createDivsForColors(shuffledColors);
 
-
-
-
-  // when the DOM loads
-  createDivsForColors(shuffledColors);
-})
-
-
+/* */
