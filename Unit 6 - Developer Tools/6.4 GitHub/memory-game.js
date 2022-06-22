f50e0c99e3ade811b25b463c512db3bd219d060a
Alex Howard <alexamh@zoho.com>
2022-06-22 19:44:04 -0400

    Added memory Game to Springboard Repo

---
+++ Unit 6 - Developer Tools/6.4 GitHub/memory-game.js
@@ -0,0 +1,117 @@
+const gameContainer = document.getElementById("game");
+
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+// TODO: Implement this function!
+function handleCardClick(event) {
+  // you can use event.target to see which element was clicked
+  console.log("you just clicked", event.target);
+  if (noClicking) return;
+  if (event.target.classList.contains("flipped")) return;
+
+  let currentCard = event.target;
+  currentCard.style.backgroundColor = currentCard.classList[0];
+
+  if (!card1 || !card2) {
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    if (currentCard === card1) {
+      card2 = null;
+    } else {
+      card2 = currentCard;
+    }
+
+    if (card1 && card2) {
+      noClicking = true;
+      
+      if (card1.className === card2.className) {
+        cardsFlipped += 2;
+        card1.removeEventListener("click", handleCardClick);
+        card2.removeEventListener("click", handleCardClick);
+        card1 = null;
+        card2 = null;
+        noClicking = false;
+      } else {
+       const timer = setTimeout(function () {
+          card1.style.backgroundColor = 'white';
+          card2.style.backgroundColor = 'white';
+          card1.classList.remove("flipped");
+          card2.classList.remove("flipped");
+          card1 = null;
+          card2 = null;
+          noClicking = false;
+        }, 1000)
+    }
+  }
+
+  }
+  if (cardsFlipped === COLORS.length) {
+    alert('GAME OVER')
+  }
+
+}
+
+// when the DOM loads
+createDivsForColors(shuffledColors);
+
+/* */
