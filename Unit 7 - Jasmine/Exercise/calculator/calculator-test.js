cee78c9c7225ceb9fa75d02fc57873a4565510ff
Alex Howard <alexamh@zoho.com>
2022-06-24 17:19:08 -0400

    Finished Calculator Testing exercise using Jasmine

--- Unit 7 - Jasmine/Exercise/calculator/calculator-test.js
+++ Unit 7 - Jasmine/Exercise/calculator/calculator-test.js
@@ -1,11 +1,18 @@
+describe('Calculate Monthly Rate', function () {
+  it("should calculate the monthly rate correctly", function () {
+    expect(calculateMonthlyPayment({ "amount": 60000, "years": 5, "rate": 0 })).toEqual('1000.00')
+    expect(calculateMonthlyPayment({ "amount": 1250, "years": 1, "rate": .50 })).toEqual('104.69')
+  });
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+  it("should return a result with 2 decimal places", function () {
+    expect(calculateMonthlyPayment({ "amount": 100000, "years": 5, "rate": 3.5 })).toEqual('1725.00')
+    expect(calculateMonthlyPayment({ "amount": 777777, "years": 7, "rate": 7.7 })).toEqual('9972.21')
+  });
+
+  it("should catch invalid inputs", function () {
+    expect(calculateMonthlyPayment({ "amount": 'hello', "years": 5, "rate": 3.5 })).toEqual('Invalid Input')
+    expect(calculateMonthlyPayment({ "amount": 125000, "years": -5, "rate": 3.5 })).toEqual('Invalid Input')
+    expect(calculateMonthlyPayment({ "amount": 125000, "years": 0, "rate": 25 })).toEqual('Invalid Input')
+  });
+
 });
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
--- Unit 7 - Jasmine/Exercise/calculator/calculator.js
+++ Unit 7 - Jasmine/Exercise/calculator/calculator.js
@@ -21,20 +21,43 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let amount = 100000;
+  let years = 5;
+  let rate = 6;
+  
+  document.getElementById("loan-amount").value = amount;
+  document.getElementById("loan-years").value = years;
+  document.getElementById("loan-rate").value = rate;
+
+  let monthly = calculateMonthlyPayment({"amount":amount, "years":years, "rate":rate})
+  updateMonthly(monthly)
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let values = getCurrentUIValues();
+  let monthlyPayment = calculateMonthlyPayment(values);
+  updateMonthly(monthlyPayment);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+    const amount = values["amount"];
+    const years = values["years"];
+    const rate = values["rate"];
+  
+    let monthly = ((amount * (rate * .01) + amount) / (years * 12)).toFixed(2);
+    if (monthly === 'Infinity' || monthly === 'NaN' || !monthly || monthly < 0){return 'Invalid Input'}
+
+    return monthly;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  let span = document.getElementById("monthly-payment")
+  span.innerText = `$${monthly}`;
 }
