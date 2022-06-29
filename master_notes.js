06818d9740462b8b8f095e234ed512080a1800ce
Alex Howard <alexamh@zoho.com>
2022-06-29 16:38:40 -0400

    Updated notes through Unit 8.3 - Reduce

--- master_notes.js
+++ master_notes.js
@@ -2011,4 +2011,92 @@ A better version of indexOf to be used when a callback is necessary
 
 
 
+**** reduce ****
+
+Whatever is returned from the callback function, becomes the new value of the accumulator!
+
+Accepts a callback function and an optional second parameter
+Iterates through an array
+Runs a callback on each value in the array
+The first parameter to the callback is either the first value in the array or the optional second parameter
+The first parameter to the callback is often called “accumulator”
+The returned value from the callback becomes the new value of accumulator
+Let’s Break It Down
+
+let evens = [2,4,6,8,10];
+
+evens.reduce(function(accumulator, nextValue){
+  return accumulator + nextValue;
+});
+
+
+  2
+  6
+  12
+  20
+  30
+
+
+Adding A Second Parameter
+
+let evens = [2,4,6,8,10];
+
+evens.reduce(function(accumulator, nextValue){
+  return accumulator + nextValue;
+},10);
+
+
+  12
+  16
+  22
+  30
+  40
+
+
+Let’s Try Something Else
+
+let names = ['Maya', 'Tammy', 'Angela', 'Alexis'];
+
+names.reduce(function(accumulator, nextValue){
+  if(nextValue !== "Colt"){
+    return accumulator += ' ' + nextValue;
+  }
+  return accumulator;
+},'My friends are');
+
+
+  Here is what reduce will build up:
+
+  'My friends are Maya'
+  'My friends are Maya Tammy'
+  'My friends are Maya Tammy Angela'
+
+  With a final output of:
+
+  'My friends are Maya Tammy Angela Alexis'
+
+
+When You Would Use Reduce
+
+It works for almost everything, but is sometimes overkill
+When you want to transform an array into another data structure
+
+
+EXAMPLE
+function vowelCount(str){
+    const vowels = "aeiou";
+    return str.split('').reduce(function(acc,next){        //str.split turns into array of characters
+        let lowerCased = next.toLowerCase()              
+        if(vowels.indexOf(lowerCased) !== -1){             // If an index is found, means vowel exists
+            if(acc[lowerCased]){                           // The accumulator is an object!, if the 'key' exists (the vowel)
+                acc[lowerCased]++;                         // Key exists, add one to value in object
+            } else {
+                acc[lowerCased] = 1;                       // Key doesn't exist, so add vowel to object with value 1
+            }
+        }
+        return acc;                                        // return this object each time
+    }, {});                                                // Initializer value is an empty object
+}
+
+
 */
