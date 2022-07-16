54e0a23d868b198b9bf3a3390cf943b6471e7e1b
Alex Howard <alexamh@zoho.com>
2022-07-16 11:16:14 -0400

    Started Exercise using GIPHY API

---
+++ Section 1/Unit 14 - How the web works/14.3 - Axios/exercise/app.js
@@ -0,0 +1,41 @@
+
+const api_key = 'j1u39h1te703BwYOw4lg1lVL5PT0GMa7' 
+const searchEndpoint = 'http://api.giphy.com/v1/gifs/search'
+const searchButton = document.querySelector("#search-button")
+const removeButton = document.querySelector("#remove-images")
+
+searchButton.addEventListener("click", (event) => {
+    event.preventDefault;
+    const search = document.querySelector("#search-box").value; 
+    if (search) {
+        document.querySelector("#search-box").value = "";
+        searchGiphy(search);    
+    }
+    return
+})
+
+removeButton.addEventListener("click", (event) => {
+    event.preventDefault;
+    let gifs = document.querySelectorAll(".gif");
+    for (let gif of gifs) {
+        gif.remove();
+    }
+})
+
+
+
+const searchGiphy = async (q) => {   
+    const params = { api_key, q }
+    addGif(await axios.get(searchEndpoint, { params }));
+}
+
+const addGif = (response) => {
+    const url = response.data.data[0].url
+    let img = document.createElement("img");
+    img.src = url;
+    img.classList.add("col-3");
+    img.classList.add("gif");
+    document.querySelector("#gif-area").append(img)
+    console.log(url)
+}
+
