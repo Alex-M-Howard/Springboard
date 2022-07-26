6ea2c817de4de70cbbbc8b10e59bfa9279deb093
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 11:23:23 -0400

    Update vowel_count.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/26_vowel_count/vowel_count.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/26_vowel_count/vowel_count.py
@@ -7,3 +7,15 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    VOWELS = 'aeiou'
+    phrase = phrase.lower()
+    vowel_map = {}  
+  
+    for char in phrase:
+        if char in VOWELS:
+            if char in vowel_map: vowel_map[char] = vowel_map[char] + 1
+            vowel_map[char] = 1
+          
+    return vowel_map
+
+print(vowel_count('HOW ARE YOU? i am great!'))
