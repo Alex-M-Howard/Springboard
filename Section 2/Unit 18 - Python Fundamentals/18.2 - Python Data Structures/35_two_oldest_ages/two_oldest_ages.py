7aba55ff3664ad955c8597b0f5d798de4d0be79c
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 12:35:41 -0400

    Update two_oldest_ages.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/35_two_oldest_ages/two_oldest_ages.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/35_two_oldest_ages/two_oldest_ages.py
@@ -21,3 +21,10 @@ def two_oldest_ages(ages):
     # you may find it helpful to research the `sorted(iter)` function, which
     # can take *any* type of list-like-thing, and returns a new, sorted list
     # from it.
+    ages = list(set(ages))
+    ages.sort()
+    return (ages[-2], ages[-1])
+    
+print(two_oldest_ages([1,2,10,8]))
+print(two_oldest_ages([6,1,9,10,4]))
+print(two_oldest_ages([1,5,5,2]))
