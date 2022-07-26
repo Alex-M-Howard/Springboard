420abbc4dccf9cb372a847966eb4c9cac4e1009f
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 13:27:38 -0400

    Update read_file_list.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/fs_5_read_file_list/read_file_list.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/fs_5_read_file_list/read_file_list.py
@@ -16,6 +16,9 @@ def read_file_list(filename):
     It will raise an error if the file cannot be found.
     """
 
-    # hint: when you read lines of files, there will be a "newline"
-    # (end-of-line character) at the end of each line, and you want to
-    # strip that off before you print it. Do some research on that!
+    with open(filename) as doc:
+        for line in doc:
+            # remove newline at end of line!
+            line = line.strip()
+            print(f"- {line}")
+
