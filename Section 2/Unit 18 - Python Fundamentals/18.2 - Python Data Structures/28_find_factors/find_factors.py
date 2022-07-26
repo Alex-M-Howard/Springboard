b2f125712d56c8be7e49da5bebbf26d07af79ede
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 11:24:05 -0400

    Update find_factors.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/28_find_factors/find_factors.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/28_find_factors/find_factors.py
@@ -13,3 +13,11 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+    new_list = [n for n in range(1,num+1) if num % n == 0]
+    return new_list
+  
+  
+print(find_factors(10))
+print(find_factors(11))
+print(find_factors(111))
+print(find_factors(321421))
