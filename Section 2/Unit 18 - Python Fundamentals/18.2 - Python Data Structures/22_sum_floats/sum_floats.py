e9f7b17dd9a2613e1d0aa7f3acbee85985fa2c85
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 11:22:13 -0400

    Update sum_floats.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/22_sum_floats/sum_floats.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/22_sum_floats/sum_floats.py
@@ -7,7 +7,15 @@ def sum_floats(nums):
         >>> sum_floats([1, 2, 3])
         0
     """
+    
+    sum = 0
+    for num in nums:
+        if isinstance(num, float): sum += num
+    return sum
 
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
     # if something is a float!
+
+print(sum_floats([1.5,2.4,'awesome',[],1]))
+print(sum_floats([1,2,3]))
