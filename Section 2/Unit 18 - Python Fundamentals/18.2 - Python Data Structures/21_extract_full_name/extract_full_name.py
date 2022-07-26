9f858ca0fca765288f92cf9b88b1fa1335a4e681
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 11:21:49 -0400

    Update extract_full_name.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/21_extract_full_name/extract_full_name.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/21_extract_full_name/extract_full_name.py
@@ -13,4 +13,13 @@ def extract_full_names(people):
 
         >>> extract_full_names(names)
         ['Ada Lovelace', 'Grace Hopper']
-    """
+    # """
+    
+    return [f'{name["first"]} {name["last"]}' for name in names]
+    
+names = [
+    {'first': 'Ada', 'last': 'Lovelace'},
+    {'first': 'Grace', 'last': 'Hopper'},
+         ]
+
+print(extract_full_names(names))
