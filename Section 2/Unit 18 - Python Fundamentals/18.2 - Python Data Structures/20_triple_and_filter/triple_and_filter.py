0eabf0ebefdee6533829cf373bf53926d94a305f
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 11:21:26 -0400

    Update triple_and_filter.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/20_triple_and_filter/triple_and_filter.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/20_triple_and_filter/triple_and_filter.py
@@ -13,3 +13,8 @@ def triple_and_filter(nums):
         >>> triple_and_filter([1, 2])
         []
     """
+    return [num * 3 for num in nums if num % 4 == 0]
+
+print(triple_and_filter([1,2,3,4]))
+print(triple_and_filter([6,8,10,12]))
+print(triple_and_filter([1,2]))
