218f948f380dabd7029e75a618ff1682b49b6f66
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-26 10:05:09 -0400

    Update friend_date.py

--- Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/19_friend_date/friend_date.py
+++ Section 2/Unit 18 - Python Fundamentals/18.2 - Python Data Structures/19_friend_date/friend_date.py
@@ -16,7 +16,16 @@ def friend_date(a, b):
         >>> friend_date(sauron, gandalf)
         True
     """
-    elmo = ('Elmo', 5, ['hugging', 'being nice'])
-    sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
-    gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
     
+    for each in a[2]:
+        if each in b[2]: return True
+        
+    return False
+    
+elmo = ('Elmo', 5, ['hugging', 'being nice'])
+sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+    
+
+print(friend_date(elmo, sauron))
+print(friend_date(sauron, gandalf))
