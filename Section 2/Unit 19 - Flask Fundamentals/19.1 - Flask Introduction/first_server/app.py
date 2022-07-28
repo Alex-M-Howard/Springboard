c7b09d7b33d7baeba890814b11113401604b4e2a
Alex Howard <alexamh@zoho.com>
2022-07-28 12:47:42 -0400

    Finished some notes on simple routing in Flask

--- Section 2/Unit 19 - Flask Fundamentals/19.1 - Flask Introduction/first_server/app.py
+++ Section 2/Unit 19 - Flask Fundamentals/19.1 - Flask Introduction/first_server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -24,3 +24,42 @@ def landing_page():
         </html>
     """
     return html
+
+@app.route('/search')
+def search():
+    term = request.args["term"]
+    return f"<h1>Search Results For: {term}</h1>"
+
+
+@app.route("/add-comment")
+def add_comment_form():
+    return """
+    <h1>Add Comment</h1>
+    <form method="POST">
+        <input type='text' placeholder='comment' name='comment'/>
+        <button>Submit</button>
+    </form>
+"""
+
+@app.route("/add-comment", methods=["POST"])
+def save_comment():
+    comment = request.form["comment"]
+    
+    return f"""
+    <h1>Saved your comment: {comment}</h1>
+"""
+
+@app.route('/r/<subreddit>')
+def show_subreddit(subreddit):
+    return "This is a subreddit"
+
+POSTS = {
+    1: "I like chicken tenders",
+    2: "I hate mayo!",
+    3: "Double rainbow all the way",
+}
+
+@app.route('/posts/<int:id>')
+def find_id(id):
+    post = POSTS[id]
+    return post
