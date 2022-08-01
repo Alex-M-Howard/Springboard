8f0e3c959d5f3c9511c101612f786b418d53d5ef
Alex Howard <alexamh@zoho.com>
2022-08-01 19:37:45 -0400

    Finished Steps 1-8 of 19.3 exercise

---
+++ Section 2/Unit 19 - Flask Fundamentals/19.3 - Flask Tools/exercise/app.py
@@ -0,0 +1,49 @@
+from pickletools import read_unicodestring4
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "ASDF(!@%#$LOHASF)!@#$11240895671"
+debug = DebugToolbarExtension(app)
+
+USER_RESPONSES = []
+QUESTIONS = []
+
+for each in satisfaction_survey.questions:
+    QUESTIONS.append({
+        "question": each.question,
+        "choices": each.choices,
+    }) 
+
+@app.route('/')
+def home_page():
+    """Show start page with name of survey, instructions, and a start button."""
+    instructions = satisfaction_survey.instructions
+    title = satisfaction_survey.title
+    
+    return render_template("start.html", instructions=instructions, title=title)
+
+
+@app.route('/survey')
+def survey_page():
+    """Show survey questions"""
+    if len(QUESTIONS) > 0:
+        question = QUESTIONS[0]["question"]
+        choices = QUESTIONS[0]["choices"]
+        title = satisfaction_survey.title
+        
+        return render_template("survey.html", title=title, question=question, choices=choices)
+    else:
+        return render_template("thanks.html")
+
+
+
+@app.route('/answer', methods=["POST", "GET"])
+def store_answer():
+    """Store survey answers"""
+    if request.method == "POST":
+        USER_RESPONSES.append(request.form["answer"])
+        QUESTIONS.pop(0)
+        
+    return redirect("/survey")
