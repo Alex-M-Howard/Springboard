f38b909fff39cbab798326b492a4d42ae0936d48
Alex Howard <alexamh@zoho.com>
2022-08-04 21:14:59 -0400

    Thumbing around starter code

--- Section 2/Unit 19 - Flask Fundamentals/19.5 - Flask Testing/boggle/app.py
+++ Section 2/Unit 19 - Flask Fundamentals/19.5 - Flask Testing/boggle/app.py
@@ -1,3 +1,14 @@
 from boggle import Boggle
+from flask import Flask, request, session, render_template, redirect
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secretKey"
 
 boggle_game = Boggle()
+
+
+@app.route("/")
+def home_page():
+    
+    return render_template("start.html")
