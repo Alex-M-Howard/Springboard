62cfebdb221764a623cc77ca5702f7dbf12dc31f
Alex-Howard <alex-m-howard@pm.me>
2022-08-07 16:43:41 -0400

    routes created to strat session and redirect to boggle

--- Section 2/Unit 19 - Flask Fundamentals/19.5 - Flask Testing/boggle/app.py
+++ Section 2/Unit 19 - Flask Fundamentals/19.5 - Flask Testing/boggle/app.py
@@ -1,14 +1,37 @@
 from boggle import Boggle
-from flask import Flask, request, session, render_template, redirect
-
+from flask import Flask, request, session, render_template, redirect, url_for
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretKey"
-
-boggle_game = Boggle()
+#debug = DebugToolbarExtension(app)
 
 
 @app.route("/")
 def home_page():
-    
+    """Return start page"""
     return render_template("start.html")
+
+@app.route("/session", methods=["POST"])
+def start_session():
+    """Initialize session data when clicking start button"""
+    if request.method == "POST":
+        # Init Boggle board and score
+        boggle = Boggle()
+        board = boggle.make_board()
+        
+        session["board"] = board
+        session["score"] = 0
+        
+        print("###############################")
+        print(session["board"])
+        print("Score:", session["score"])
+        print("###############################")
+    return redirect("/boggle")
+
+@app.route("/boggle")
+def boggle():
+    """Display Boggle Game and Score"""
+    
+    
+    return render_template("game.html", board=session["board"], score=session["score"])
