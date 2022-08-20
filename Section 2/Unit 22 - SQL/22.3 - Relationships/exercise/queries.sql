fed2c761a6b8ea8c762f75d2d1b9e8c0bb9feb37
Alex Howard <alex-m-howard@pm.me>
2022-08-20 14:25:38 -0400

    Finished exercise for 22.3

--- Section 2/Unit 22 - SQL/22.3 - Relationships/exercise/queries.sql
+++ Section 2/Unit 22 - SQL/22.3 - Relationships/exercise/queries.sql
@@ -1,1 +1,24 @@
 -- write your queries here
+--1
+SELECT * 
+    FROM owners 
+    FULL JOIN vehicles 
+    ON owners.id = vehicles.owner_id;
+
+
+--2
+SELECT o.first_name, o.last_name, COUNT(v.owner_id) AS count
+    FROM owners o
+    JOIN vehicles v
+    ON o.id = v.owner_id
+    GROUP BY o.first_name, o.last_name
+    ORDER BY count;
+
+--3
+SELECT o.first_name, o.last_name, ROUND(AVG(v.price)::numeric, 0) AS average_price, COUNT(v.owner_id) AS count
+    FROM owners o
+    JOIN vehicles v
+    ON o.id = v.owner_id
+    GROUP BY o.first_name, o.last_name
+    HAVING COUNT(v.owner_id) > 1 AND AVG(v.price) > 10000
+    ORDER BY count DESC;
