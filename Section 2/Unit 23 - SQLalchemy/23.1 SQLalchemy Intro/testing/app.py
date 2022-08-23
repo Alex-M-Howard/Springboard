3e755c0b7513d5e895183b83812448097beaea50
Alex-RaspPi <alex-m-howard@pm.me>
2022-08-23 13:33:23 -0400

    follow along code

--- Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/testing/app.py
+++ Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/testing/app.py
@@ -1,26 +1,20 @@
 """Demo app using SQLAlchemy."""
-
 from flask import Flask, request, redirect, render_template, flash, session
-from flask_sqlalchemy import SQLAlchemy
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///playstore'
-
-
-db = SQLAlchemy()
-db.app = app
-db.init_app(app)
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
 app.config['SECRET_KEY'] = "SECRET!"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
-
-
+connect_db(app)
 
 @app.route("/")
 def home():
     """List pets and show add form."""
-
-   
-    return render_template("home.html")
+    print('were home')
+    all_pets = Pet.query.all()
+    
+    return render_template("home.html", pets=all_pets)
 
