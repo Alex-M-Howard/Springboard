7e0e2a9e7fe9694c4a82b154b940bab88caf1032
Alex Howard <alex-m-howard@pm.me>
2022-08-23 21:51:09 -0400

    started work on Blogly

--- Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/exercise/app.py
+++ Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/exercise/app.py
@@ -1,12 +1,37 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, request, flash, render_template, session
+from models import add_user, get_all_users, get_user, connect_db, db
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY']="password1234Q!(#asdf12348"
 
 connect_db(app)
 db.create_all()
+
+@app.route("/")
+def home():
+  """Landing page showing all users"""
+ 
+  users = get_all_users()
+  return render_template("users.html", users=users)
+
+@app.route("/user<int:id>")
+def show_user(id):
+  """Show user info"""
+  user = get_user(id)
+  return render_template(f"user.html", user=user)
+    
+  
+@app.route("/add-user", methods=["POST", "GET"])
+def add_form():
+  """Show userform or add user to db"""
+  if request.method == "GET" : return render_template("add.html")
+  else:
+    r = request
+    add_user(first_name=r.form["first_name"], last_name=r.form["last_name"], image_url=r.form["image_url"])
+    return redirect("/")
--- Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/exercise/models.py
+++ Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/exercise/models.py
@@ -1,1 +1,41 @@
-"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+  db.app = app
+  db.init_app(app)
+  
+class User(db.Model):
+
+  __tablename__ = 'users' 
+  
+  id = db.Column(db.Integer,
+                 primary_key=True,
+                 autoincrement=True)
+
+  first_name = db.Column(db.String(25), nullable=False)
+  last_name = db.Column(db.String(25), nullable=False)
+  image_url = db.Column(db.String(255), default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+
+  def __repr__(self):
+    """User description in console"""
+    u = self
+    return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}"
+
+  def get_all_users(self):
+    """Get list of users"""
+    return User.query.all()
+  
+  @classmethod  
+  def get_user(id):
+    """return user"""
+    return User.query.get(id)
+  
+  @classmethod
+  def add_user(first_name, last_name, image_url):
+    """Add user to database"""
+  
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
