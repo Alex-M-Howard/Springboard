89a49533c50d2271d88f0f229c698b4f2803306b
Alex Howard <alex-m-howard@pm.me>
2022-08-25 17:17:49 -0400

    Changed link limit to 300 instead of 255

--- Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/exercise/models.py
+++ Section 2/Unit 23 - SQLalchemy/23.1 SQLalchemy Intro/exercise/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
 
   first_name = db.Column(db.String(25), nullable=False)
   last_name = db.Column(db.String(25), nullable=False)
-  image_url = db.Column(db.String(255), default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+  image_url = db.Column(db.String(300), default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
 
   def __repr__(self):
     """User description in console"""
