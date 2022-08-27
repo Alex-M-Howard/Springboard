18663a1c26e480c9ba157699cd7d22cffad915a0
Alex Howard <alex-m-howard@pm.me>
2022-08-27 11:34:32 -0400

    Added blogly exercise to unit 23.2

---
+++ Section 2/Unit 23 - SQLalchemy/23.2 SQLalchemy Associations/exercise/app.py
@@ -0,0 +1,74 @@
+"""Blogly application."""
+
+from flask import Flask, redirect, request, flash, render_template, session
+from models import connect_db, db, User
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SECRET_KEY'] = "SECRET!"
+
+connect_db(app)
+db.create_all()
+
+
+@app.route("/")
+def home():
+  """Landing page. Redirect for now"""
+ 
+  return redirect("/users")
+
+@app.route("/users")
+def show_users():
+  """Show list of users"""
+  users = User.query.order_by('last_name', 'first_name').all()
+  return render_template("users.html", users=users)
+
+@app.route("/users/<int:id>")
+def show_user(id):
+  """Show user info"""
+  user = User.query.get_or_404(id)
+  return render_template(f"user.html", user=user)
+    
+@app.route("/users/new", methods=["POST", "GET"])
+def add_form():
+  """Show userform or add user to db"""
+  if request.method == "GET" : return render_template("new.html")
+  else:
+    r = request
+
+    fname = r.form["first_name"]
+    lname = r.form["last_name"]
+    img  = r.form["image_url"] if r.form["image_url"] else None
+
+    new_user = User(first_name=fname, last_name=lname, image_url=img)
+    new_user.add_user()
+    
+    return redirect("/")
+  
+@app.route("/users/<int:id>/edit", methods=["POST", "GET"])
+def edit_user(id):
+  """Show edit page"""
+  if request.method == "GET" : 
+    user = User.query.get_or_404(id)
+    return render_template("edit.html", user=user)
+  else:
+    r = request    
+    user = User.query.get_or_404(id)
+    user.first_name = r.form["first_name"] if r.form["first_name"] else user.first_name
+    user.last_name = r.form["last_name"] if r.form["last_name"] else user.last_name
+    user.image_url = r.form["image_url"] if r.form["image_url"] else None
+    
+    user.edit_user()
+    return redirect('/users')
+
+@app.route("/users/<int:id>/delete", methods=["POST"])
+def delete_user(id):
+  """Delete user"""
+  
+  User.delete_user(id)
+  flash("User successfully deleted!", 'success')
+
+  return redirect("/users")
---
+++ Section 2/Unit 23 - SQLalchemy/23.2 SQLalchemy Associations/exercise/models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+  db.app = app
+  db.init_app(app)
+  
+class User(db.Model):
+
+  __tablename__ = 'users' 
+  
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  first_name = db.Column(db.String(25), nullable=False)
+  last_name = db.Column(db.String(25), nullable=False)
+  image_url = db.Column(db.String(300), default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+
+  def __repr__(self):
+    """User description in console"""
+    u = self
+    return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}"
+  
+  def add_user(self):
+    """Add user to DB"""
+    db.session.add(self)
+    db.session.commit()
+    
+  def edit_user(self):
+    """Edit current user"""
+    db.session.add(self)
+    db.session.commit()
+    
+  def get_full_name(self):
+    return  f"{self.first_name} {self.last_name}"
+  
+  @classmethod  
+  def delete_user(cls, user_id):
+    """Delete user from DB"""
+    cls.query.filter_by(id=user_id).delete()
+    db.session.commit()
