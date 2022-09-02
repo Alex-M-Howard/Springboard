052f1d5cfdc032a959fde86a048a2e799a9d7cdf
Alex Howard <alex-m-howard@pm.me>
2022-09-02 17:00:26 -0400

    Corrected form labels

--- Section 2/Unit 24 - Intermediate Flask/24.1 - WTForms/exercise/forms.py
+++ Section 2/Unit 24 - Intermediate Flask/24.1 - WTForms/exercise/forms.py
@@ -1,32 +1,18 @@
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import InputRequired, Optional
+
 
 class AddPetForm(FlaskForm):
     """Form for adding new pets."""
 
     name = StringField("Pet Name", validators=[InputRequired()])
-    spieces = StringField("Species" validators=[InputRequired()])
-    photo_url = StringField("Species" validators=[Optional()])
-    age = IntegerField("Species" validators=[InputOptional()])
-    notes = StringField("Species" validators=[InputOptional()])
+    species = StringField("Species", validators=[InputRequired()])
+    photo_url = StringField("Photo URL", validators=[Optional()])
+    age = IntegerField("Age", validators=[Optional()])
+    notes = StringField("Notes", validators=[Optional()])
     
     
-@app.route("/add", methods=["GET", "POST"])
-def add_pet():
-    """Show add pet form; Add pet to DB"""
-    form = AddPetForm()
-  
-    if form.validate_on_submit():
-      name = form.name.data
-      species = form.species.data
-      photo_url = form.photo_url.data
-      age = form.age.data
-      notes = form.notes.data
-      flask(f"{name} added!")
-      return redirect("/")
-  
-    else:
-      return render_template("add_pet_form.html", form=form)
-    
+
     
