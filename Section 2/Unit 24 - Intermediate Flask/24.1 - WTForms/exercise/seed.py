52d7566d2a445fb3c13a49159c8cc170fc6089f0
Alex Howard <alex-m-howard@pm.me>
2022-09-03 09:23:44 -0400

    Refactored photo links to new folder. Species validation

--- Section 2/Unit 24 - Intermediate Flask/24.1 - WTForms/exercise/seed.py
+++ Section 2/Unit 24 - Intermediate Flask/24.1 - WTForms/exercise/seed.py
@@ -5,13 +5,13 @@ def seed(db):
     db.create_all()
     
     #Add pets
-    p1 = Pet(name='Woody', species='dog', photo_url='/static/woody.jpg', age=5, notes='Loves rawhides and drinking out of toilets.', available=False)
-    p2 = Pet(name='Nolan', species='dog', photo_url='/static/nolan.jpg', age=13, notes='Loves to sleep and smells like smelly feet.', available=False)
-    p3 = Pet(name='Sally', species='cat', photo_url='/static/sally.jpg', age=9, notes='Loves to cuddle and run.', available=True)
-    p4 = Pet(name='Tippy', species='cat', photo_url='/static/tippy.jpg', age=8, notes='Loves to sit in the corner and judge silently.', available=False)
-    p5 = Pet(name='Lenny Penny', species='chicken', photo_url='/static/lennypenny.jpg', age=1, notes='Vaccinations up to date.', available=True)
-    p6 = Pet(name='Thumper', species='rabbit', photo_url='/static/thumper.jpg', age=2, notes='Located in Birdsboro.', available=True)
-    p7 = Pet(name='Lightning', species='gerbil', notes='Super fast.', available=True)
+    p1 = Pet(name='Woody', species='DOG', photo_url='/static/images/woody.jpg', age=5, notes='Loves rawhides and drinking out of toilets.', available=False)
+    p2 = Pet(name='Nolan', species='DOG', photo_url='/static/images/nolan.jpg', age=13, notes='Loves to sleep and smells like smelly feet.', available=False)
+    p3 = Pet(name='Sally', species='CAT', photo_url='/static/images/sally.jpg', age=9, notes='Loves to cuddle and run.', available=True)
+    p4 = Pet(name='Tippy', species='CAT', photo_url='/static/images/tippy.jpg', age=8, notes='Loves to sit in the corner and judge silently.', available=False)
+    p5 = Pet(name='Lenny Penny', species='PORCUPINE', photo_url='/static/images/lennypenny.jpg', age=1, notes='Vaccinations up to date.', available=True)
+    p6 = Pet(name='Thumper', species='PORCUPINE', photo_url='/static/images/thumper.jpg', age=2, notes='Located in Birdsboro.', available=True)
+    p7 = Pet(name='Lightning', species='PORCUPINE', notes='Super fast.', available=True)
     
     
     db.session.add_all([p1, p2, p3, p4, p5, p6, p7])
