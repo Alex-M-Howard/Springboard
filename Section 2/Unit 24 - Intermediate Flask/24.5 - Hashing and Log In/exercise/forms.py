21e06fbf43b21b7c4aed8b5b8e803aaa9a028261
Alex-RaspPi <alex-m-howard@pm.me>
2022-09-07 08:10:00 -0400

    Added error messages for validators

--- Section 2/Unit 24 - Intermediate Flask/24.5 - Hashing and Log In/exercise/forms.py
+++ Section 2/Unit 24 - Intermediate Flask/24.5 - Hashing and Log In/exercise/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import InputRequired, Email
 
 class AddUserForm(FlaskForm):
     """Add new user form"""
-    username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("Password", validators=[InputRequired()])
-    email = StringField("Email", validators=[InputRequired(), Email()])
-    first_name = StringField("First Name", validators=[InputRequired()])
-    last_name = StringField("Last Name", validators=[InputRequired()])
+    username = StringField("Username", validators=[InputRequired(message='You must enter a username')])
+    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password')])
+    email = StringField("Email", validators=[InputRequired(), Email(granular_message=True)])
+    first_name = StringField("First Name", validators=[InputRequired(message='Please enter your first name')])
+    last_name = StringField("Last Name", validators=[InputRequired(message='Please enter your last name')])
