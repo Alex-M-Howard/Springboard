e9f6df4182f2fb4f73f07a8d9bd048f55d9fa9a9
Alex-RaspPi <alex-m-howard@pm.me>
2022-09-07 10:10:23 -0400

    Added login form

--- Section 2/Unit 24 - Intermediate Flask/24.5 - Hashing and Log In/exercise/forms.py
+++ Section 2/Unit 24 - Intermediate Flask/24.5 - Hashing and Log In/exercise/forms.py
@@ -10,3 +10,9 @@ class AddUserForm(FlaskForm):
     email = StringField("Email", validators=[InputRequired(), Email(granular_message=True)])
     first_name = StringField("First Name", validators=[InputRequired(message='Please enter your first name')])
     last_name = StringField("Last Name", validators=[InputRequired(message='Please enter your last name')])
+    
+class UserLoginForm(FlaskForm):
+    """User Login form"""
+    username = StringField("Username", validators=[InputRequired(message='You must enter a username')])
+    password = PasswordField("Password", validators=[InputRequired(message='You must enter a password')])
+    
