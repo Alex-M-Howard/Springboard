75e3b89b1a7ae6c13d3b5f93b6d409f204cca4a5
Alex Howard <alex-m-howard@pm.me>
2022-09-13 20:29:13 -0400

    Added edit profile form

--- Section 2/Unit 26 - Twitter Clone/forms.py
+++ Section 2/Unit 26 - Twitter Clone/forms.py
@@ -23,3 +23,14 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class EditForm(FlaskForm):
+    """User Profile Edit"""
+    
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    
+    
