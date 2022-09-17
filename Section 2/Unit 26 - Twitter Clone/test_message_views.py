0d7305a7b7b2acfc56103d8cdcbf14a6cb424c53
Alex Howard <alex-m-howard@pm.me>
2022-09-17 14:28:00 -0400

    Added unit testing for logged out user

--- Section 2/Unit 26 - Twitter Clone/test_message_views.py
+++ Section 2/Unit 26 - Twitter Clone/test_message_views.py
@@ -19,7 +19,7 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 
 # Now we can import app
-
+    
 from app import app, CURR_USER_KEY
 
 # Create our tables (we do this here, so we only create the tables
@@ -58,13 +58,6 @@ class MessageViewTestCase(TestCase):
 
         db.session.commit()
 
-        message = Message(text="HELLO TEST", user_id= self.testuser2.id)
-        db.session.add(message)
-        db.session.commit()
-        
-    def test_add_message(self):
-        """Can use add a message?"""
-
         # Since we need to change the session to mimic logging in,
         # we need to use the changing-session trick:
 
@@ -75,92 +68,155 @@ class MessageViewTestCase(TestCase):
             # Now, that session setting is saved, so we can have
             # the rest of ours test
 
-            resp = c.post("/messages/new", data={"text": "Hello"})
+        
+    def test_add_message(self):
+        """Can use add a message?"""
 
-            # Make sure it redirects
-            self.assertEqual(resp.status_code, 302)
-
-            msg = Message.query.filter_by(text="Hello").one()
-            self.assertEqual(msg.text, "Hello")
-
+        resp = self.client.post("/messages/new", data={"text": "Hello"})
+        msg = Message.query.filter_by(text="Hello").one()
+      
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(msg.text, "Hello")
+        
+        
+        """Are we denied if logged out?"""
+        self.client.get("/logout", follow_redirects=True)
+        
+        resp = self.client.post("/messages/new", data={"text": "Testing Testing!"}, follow_redirects=True)
+        msg = Message.query.filter_by(text="Testing Testing!").all()
+        
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(msg), 0)
+        self.assertIn('Access unauthorized', str(resp.data, 'UTF-8'))
+        
+        
     def test_show_message(self):
         """ Can we show a user's message? """
         
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = self.testuser.id
-            
-            message = Message(text="Show Me", user_id=self.testuser.id)
-            db.session.add(message)
-            db.session.commit()
-            
-            response = c.get(f"/messages/{message.id}")
-            
-            self.assertEqual(response.status_code, 200)
-            self.assertIn('<p class="single-message">Show Me</p>', str(response.data, 'UTF-8'))
+        message = Message(text="Show Me", user_id=self.testuser.id)
+        db.session.add(message)
+        db.session.commit()
+        
+        response = self.client.get(f"/messages/{message.id}")
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('<p class="single-message">Show Me</p>', str(response.data, 'UTF-8'))
+    
              
     def test_destroy_message(self):
         """ Can we delete a user's message? """
+    
+        message = Message(text="Show Me", user_id=self.testuser.id)
+        db.session.add(message)
+        db.session.commit()
         
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = self.testuser.id
+        response = self.client.post(f"/messages/{message.id}/delete")
+        message = Message.query.get(message.id)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(message, None)
+
+
+        """Are we denied if logged out?"""
+        self.client.get("/logout", follow_redirects=True)
+        
+        message = Message(text="Show Me", user_id=self.testuser.id)
+        db.session.add(message)
+        db.session.commit()
+        
+        response = self.client.post(f"/messages/{message.id}/delete", follow_redirects=True)
+        message = Message.query.get(message.id)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(message.text, "Show Me")
+        self.assertIn('Access unauthorized', str(response.data, 'UTF-8'))
             
-            message = Message(text="Show Me", user_id=self.testuser.id)
-            db.session.add(message)
-            db.session.commit()
-            
-            response = c.post(f"/messages/{message.id}/delete")
-            message = Message.query.get(message.id)
-            
-            self.assertEqual(response.status_code, 302)
-            self.assertEqual(message, None)
 
     def test_like_unlike_message(self):
         """ Can we like or unlike a user's message? """
         
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = self.testuser.id
-            
-            # Check for liked messages     
-            message = Message.query.filter_by(text="HELLO TEST").one()
-            response = c.post(f"/users/add_like/{message.id}", follow_redirects=True)
-            user = User.query.get(self.testuser.id)
-            
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(len(user.likes), 1)
-            
-            
-            # Unlike message and check again
-            message = Message.query.filter_by(text="HELLO TEST").one()
-            response = c.post(f"/users/add_like/{message.id}", follow_redirects=True)
-            user = User.query.get(self.testuser.id)
-            
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(len(user.likes), 0)           
-            
+        # Add a test message
+        user = User.query.filter_by(username='testuser2').one()
+        message = Message(text="HELLO TEST", user_id=user.id)
+        db.session.add(message)
+        db.session.commit()
+        
+        
+        # Check for liked messages     
+        response = self.client.post(f"/users/add_like/{message.id}", follow_redirects=True)
+        user = User.query.get(self.testuser.id)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(user.likes), 1)
+        
+        
+        # Unlike message and check again
+        message = Message.query.filter_by(text="HELLO TEST").one()
+        response = self.client.post(f"/users/add_like/{message.id}", follow_redirects=True)
+        user = User.query.get(self.testuser.id)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(user.likes), 0)           
+        
+        
+        
+        
+        # Are we denied if logged out?
+        self.client.get("/logout", follow_redirects=True)
+        
+        # Add a test message
+        user = User.query.filter_by(username='testuser2').one()
+        message = Message(text="UNAUTHORIZED TEST", user_id=user.id)
+        message2 = Message(text="UNAUTHORIZED UNLIKE", user_id=user.id)
+
+        user_test_user = User.query.filter_by(username='testuser').one()
+        user_test_user.likes.append(message2)
+
+        db.session.add(message)
+        db.session.commit()
+        
+        
+        # Unauthorized like messages     
+        response = self.client.post(f"/users/add_like/{message.id}", follow_redirects=True)
+        user = User.query.get(self.testuser.id)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Access unauthorized', str(response.data, 'UTF-8'))
+        self.assertEqual(len(user.likes), 1)
+        
+        
+        # Unauthorized Unlike message and check again
+        message = Message.query.filter_by(text="UNAUTHORIZED UNLIKE").one()
+        response = self.client.post(f"/users/add_like/{message.id}", follow_redirects=True)
+        user = User.query.get(self.testuser.id)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Access unauthorized', str(response.data, 'UTF-8'))
+        self.assertEqual(len(user.likes), 1)           
+        
+        
     def test_show_liked_messages(self):
         """ Can we messages we've liked? """
         
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = self.testuser.id
-            
-            # Check for liked messages     
-            message = Message.query.filter_by(text="HELLO TEST").one()
-            response = c.post(f"/users/add_like/{message.id}", follow_redirects=True)
-            user = User.query.get(self.testuser.id)
-            response = c.get(f"/users/{self.testuser.id}/likes")
-            
-            self.assertIn('<p>HELLO TEST</p>', str(response.data, 'UTF-8'))
-            
-            
-            # Unlike message and check again
-            message = Message.query.filter_by(text="HELLO TEST").one()
-            response = c.post(f"/users/add_like/{message.id}", follow_redirects=True)
-            user = User.query.get(self.testuser.id)
-            response = c.get(f"/users/{self.testuser.id}/likes")
-            
-            self.assertNotIn('<p>HELLO TEST</p>', str(response.data, 'UTF-8'))        
+        # Add a test message
+        user = User.query.filter_by(username='testuser2').one()
+        message = Message(text="HELLO TEST", user_id=user.id)
+        db.session.add(message)
+        db.session.commit()
     
+        # Check for liked messages     
+        message = Message.query.filter_by(text="HELLO TEST").one()
+        response = self.client.post(f"/users/add_like/{message.id}", follow_redirects=True)
+        user = User.query.get(self.testuser.id)
+        response = self.client.get(f"/users/{self.testuser.id}/likes")
+        
+        self.assertIn('<p>HELLO TEST</p>', str(response.data, 'UTF-8'))
+        
+        
+        # Unlike message and check again
+        message = Message.query.filter_by(text="HELLO TEST").one()
+        response = self.client.post(f"/users/add_like/{message.id}", follow_redirects=True)
+        user = User.query.get(self.testuser.id)
+        response = self.client.get(f"/users/{self.testuser.id}/likes")
+        
+        self.assertNotIn('<p>HELLO TEST</p>', str(response.data, 'UTF-8'))        
