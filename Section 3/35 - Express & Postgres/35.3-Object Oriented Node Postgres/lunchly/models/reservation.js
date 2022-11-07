dc09f823f7a35589782dfd2f80ea8a3f22cb8b97
Alex Howard <alex-m-howard@pm.me>
2022-11-07 17:29:08 -0500

    Added reservation.save() for new reservations

--- Section 3/35 - Express & Postgres/35.3-Object Oriented Node Postgres/lunchly/models/reservation.js
+++ Section 3/35 - Express & Postgres/35.3-Object Oriented Node Postgres/lunchly/models/reservation.js
@@ -8,7 +8,7 @@ const db = require("../db");
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -19,24 +19,38 @@ class Reservation {
   /** formatter for startAt */
 
   getformattedStartAt() {
-    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+    return moment(this.startAt).format("MMMM Do YYYY, h:mm a");
   }
 
   /** given a customer id, find their reservations. */
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
-    return results.rows.map(row => new Reservation(row));
+    return results.rows.map((row) => new Reservation(row));
+  }
+
+
+
+  /** save new reservation */
+
+  async save() {
+    const result = await db.query(
+      `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+            VALUES ($1, $2, $3, $4)
+            RETURNING id`,
+      [this.customerId, this.startAt, this.numGuests, this.notes]
+    );
+    this.id = result.rows[0].id;
   }
 }
 
