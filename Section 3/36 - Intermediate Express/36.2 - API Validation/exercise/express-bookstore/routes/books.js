b7d9876c7d9144097bc49863b6c527bb6ca8ebdf
Alex Howard <alex-m-howard@pm.me>
2022-12-05 17:03:20 -0500

    Finished exercise

--- Section 3/36 - Intermediate Express/36.2 - API Validation/exercise/express-bookstore/routes/books.js
+++ Section 3/36 - Intermediate Express/36.2 - API Validation/exercise/express-bookstore/routes/books.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const Book = require("../models/book");
 const jsonSchema = require('jsonschema')
-const bookSchema = require('../schemas/bookSchema.json')
+const bookSchemaNew = require('../schemas/bookSchemaNew.json')
+const bookSchemaUpdate = require('../schemas/bookSchemaUpdate.json')
 const ExpressError = require('../expressError')
 
 const router = new express.Router();
@@ -32,7 +33,7 @@ router.get("/:id", async function (req, res, next) {
 /** POST /   bookData => {book: newBook}  */
 
 router.post("/", async function (req, res, next) {
-  const result = jsonSchema.validate(req.body, bookSchema);
+  const result = jsonSchema.validate(req.body, bookSchemaNew);
 
   if (!result.valid) {
     let listOfErrors = result.errors.map(error => error.stack)
@@ -48,12 +49,16 @@ router.post("/", async function (req, res, next) {
 /** PUT /[isbn]   bookData => {book: updatedBook}  */
 
 router.put("/:isbn", async function (req, res, next) {
-  try {
+  const result = jsonSchema.validate(req.body, bookSchemaUpdate);
+
+  if (!result.valid) {
+    let listOfErrors = result.errors.map((error) => error.stack);
+    let error = new ExpressError(listOfErrors, 400);
+    return next(error);
+  }
+
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
-  } catch (err) {
-    return next(err);
-  }
 });
 
 /** DELETE /[isbn]   => {message: "Book deleted"} */
