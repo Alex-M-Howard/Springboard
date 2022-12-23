3cef2b98f76aed1eeb482f46a193258437943bc4
Alex Howard <alex-m-howard@pm.me>
2022-12-23 14:59:22 -0500

    changed jobly_test -> jobly-test

--- Section 3/37 - Cumulative Project 2 - Jobly/jobly.sql
+++ Section 3/37 - Cumulative Project 2 - Jobly/jobly.sql
@@ -8,11 +8,11 @@ CREATE DATABASE jobly;
 \i jobly-schema.sql
 \i jobly-seed.sql
 
-\echo 'Delete and recreate jobly_test db?'
+\echo 'Delete and recreate jobly-test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE jobly_test;
-CREATE DATABASE jobly_test;
-\connect jobly_test
+DROP DATABASE jobly-test;
+CREATE DATABASE jobly-test;
+\connect jobly-test
 
 \i jobly-schema.sql
