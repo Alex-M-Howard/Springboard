ef82625d1e8f271925bcddc36a9461b57ef1b774
Alex Howard <alex-m-howard@pm.me>
2022-12-23 13:12:39 -0500

    added ensureAdmin middleware

--- Section 3/37 - Cumulative Project 2 - Jobly/middleware/auth.js
+++ Section 3/37 - Cumulative Project 2 - Jobly/middleware/auth.js
@@ -3,6 +3,7 @@
 /** Convenience middleware to handle common auth cases in routes. */
 
 const jwt = require("jsonwebtoken");
+const db = require("../db");
 const { SECRET_KEY } = require("../config");
 const { UnauthorizedError } = require("../expressError");
 
@@ -21,9 +22,6 @@ async function authenticateJWT(req, res, next) {
     if (authHeader) {
       const token = authHeader.replace(/^[Bb]earer /, "").trim();
       res.locals.user = jwt.verify(token, SECRET_KEY);
-      
-      let isAdmin = await db.query(`SELECT is_admin FROM users WHERE username = $1`, [res.locals.user.username]);
-      res.locals.user.isAdmin = isAdmin.rows[0].is_admin;
     }
     return next();
   } catch (err) {
@@ -39,6 +37,22 @@ async function authenticateJWT(req, res, next) {
 function ensureLoggedIn(req, res, next) {
   try {
     if (!res.locals.user) throw new UnauthorizedError();
+
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+/** Middleware to use when they must be an admin.
+ *
+ * If not, raises Unauthorized.
+ */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user.isAdmin) throw new UnauthorizedError();
+
     return next();
   } catch (err) {
     return next(err);
@@ -46,7 +60,9 @@ function ensureLoggedIn(req, res, next) {
 }
 
 
+
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
 };
