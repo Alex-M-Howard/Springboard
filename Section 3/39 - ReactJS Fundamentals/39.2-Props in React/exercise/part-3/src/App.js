3e4322e9ce9558af497651e3ca0f926869723cd0
Alex Howard <alex-m-howard@pm.me>
2023-01-08 17:17:08 -0500

    Actually finished exercise 3

--- Section 3/39 - ReactJS Fundamentals/39.2-Props in React/exercise/part-3/src/App.js
+++ Section 3/39 - ReactJS Fundamentals/39.2-Props in React/exercise/part-3/src/App.js
@@ -4,11 +4,23 @@ import Person from './Person.js';
 function App() {
   return (
     <div className="App">
-      <Person name="Alex" age={32} />
+      <Person
+        name="Alexander"
+        age={32}
+        hobbies={["programming", "watching people", "being cursed at"]}
+      />
       <hr></hr>
-      <Person name="Lena" age={34} />
+      <Person
+        name="LenaLena"
+        age={34}
+        hobbies={["singing", "watching birds"]}
+      />
       <hr></hr>
-      <Person name="Olive" age={17} />
+      <Person
+        name="Olive"
+        age={17}
+        hobbies={["dancing", "watching cats", "cursing"]}
+      />
     </div>
   );
 }
--- Section 3/39 - ReactJS Fundamentals/39.2-Props in React/exercise/part-3/src/Person.js
+++ Section 3/39 - ReactJS Fundamentals/39.2-Props in React/exercise/part-3/src/Person.js
@@ -1,13 +1,17 @@
 const Person = (props) => {
+    let name = props.name;
 
     return (
       <div>
         <p>Learn some information about this person</p>
-        <h3>Name: {props.name}</h3>
+        <h3>Name: {name.length > 8 ? name.slice(0,6) : name}</h3>
         <h3>Age: {props.age}</h3>
         <h3>
             {props.age > 18 ? "Please go vote!" : "You must be 18"}
         </h3>
+        <ul>
+            {props.hobbies.map((val)=> <li>{val}</li>)}
+        </ul>
       </div>
     );
 }
