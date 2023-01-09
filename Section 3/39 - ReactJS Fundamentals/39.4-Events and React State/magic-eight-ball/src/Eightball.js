8ba595765e0032a727387f905f316061db028bd3
Alex Howard <alex-m-howard@pm.me>
2023-01-09 18:24:52 -0500

    Eightball project done

--- Section 3/39 - ReactJS Fundamentals/39.4-Events and React State/magic-eight-ball/src/Eightball.js
+++ Section 3/39 - ReactJS Fundamentals/39.4-Events and React State/magic-eight-ball/src/Eightball.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import "./Eightball.css"
 
-const Eightball = () => {
+const Eightball = (props) => {
+
+    const [color, setColor] = useState('blue');
+    const [msg, setMsg] = useState('Think of a question...')
+
+    function askBall(answers){
+        
+        let choice = answers[Math.floor(Math.random() * answers.length)];
+        setColor(choice.color);
+        setMsg(choice.msg);
+    };
+    
     return (
-        <div>
-            <h1>Hello</h1>
+      <div className="Eightball" onClick={() => askBall(props.answers)}>
+        <div className={`Eightball-${color}`}>
+          <h3 className="Eightball-msg">{msg}</h3>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Eightball;
