4f3f6684f58f54368e816b86156bd4572b80b1bf
Alex Howard <alex-m-howard@pm.me>
2023-01-25 20:04:56 -0500

    Finished count zeroes and sorted-freq

--- Section 5/47.3 - Divide and Conquer/exercises/count-zeroes.js
+++ Section 5/47.3 - Divide and Conquer/exercises/count-zeroes.js
@@ -1,5 +1,24 @@
-function countZeroes() {
-  
+// Given an array of 1s and 0s which has all 1s first followed by all 0s, write a function called countZeroes, which returns the number of zeroes in the array.
+
+// Constraints:
+
+// Time Complexity: O(log N)
+
+function countZeroes(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+
+        if (arr[middle] === 0) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+
+    return arr.length - left;
 }
 
 module.exports = countZeroes
--- Section 5/47.3 - Divide and Conquer/exercises/sorted-frequency.js
+++ Section 5/47.3 - Divide and Conquer/exercises/sorted-frequency.js
@@ -1,5 +1,40 @@
-function sortedFrequency() {
+function sortedFrequency(arr, target) {
+    console.log('--------------------------------');
+    let left = 0;
+    let right = arr.length - 1;
 
+    if (arr[0] > target || arr[arr.length-1] < target) return -1;
+
+    let start, last;
+
+    // Starting Index first
+    while (left < right) {
+        let middle = Math.floor((right + left) / 2);
+    
+        if (arr[middle] < target) {
+            left = middle + 1;
+        } else if (arr[middle] >= target) {
+            right = middle - 1;
+        }
+    }
+    
+    start = left;
+    right = arr.length - 1;
+    
+    // Find last index
+    while (left < right) {
+        let middle = Math.floor((right + left) / 2);
+
+        if (arr[middle] <= target) {
+            left = middle + 1;
+        } else if (arr[middle] > target) {
+            right = middle - 1;
+        }
+    }
+    last = right;
+
+    let count = last - start;
+    return count || 1;
 }
 
 module.exports = sortedFrequency
