8105161ec99a8f058f618c4e1d7133ae31766fdf
Alex Howard <alex-m-howard@pm.me>
2023-01-26 20:38:00 -0500

    Finished algorithm exercise

--- Section 5/47.3 - Divide and Conquer/exercises/find-floor.js
+++ Section 5/47.3 - Divide and Conquer/exercises/find-floor.js
@@ -1,5 +1,39 @@
-function findFloor() {
-  
+// findFloor
+// Write a function called findFloor which accepts a sorted array and a value x, and returns the floor of x in the array. 
+//The floor of x in an array is the largest element in the array which is smaller than or equal to x.If the floor does not exist, return -1.
+
+// Examples:
+
+// findFloor([1,2,8,10,10,12,19], 9) // 8
+// findFloor([1,2,8,10,10,12,19], 20) // 19
+// findFloor([1,2,8,10,10,12,19], 0) // -1
+// Constraints
+
+// Time Complexity: O(log N)
+
+
+
+
+function findFloor(arr, target) {
+    if (target < arr[0]) return -1;
+    if (target > arr[arr.length - 1]) return arr[arr.length - 1];
+
+    let left = 0, right = arr.length - 1;
+
+    while (left < right) {
+        
+        let middle = Math.floor((left + right) / 2);
+
+        if (target <= arr[middle]) {
+            right = middle - 1;
+        }
+        if (target >= arr[middle]) {
+            left = middle + 1;
+        }
+    }
+
+    return arr[left - 1];
+
 }
 
 module.exports = findFloor
--- Section 5/47.3 - Divide and Conquer/exercises/find-rotated-index.js
+++ Section 5/47.3 - Divide and Conquer/exercises/find-rotated-index.js
@@ -14,10 +14,6 @@
 // findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
 
 
-// First, find middle -> Find rotation
-// Is L < middle? Then rotation happens on right side Else Rotation happens on left side.
-
-
 
 function findRotationCount(arr) {
   if (arr[0] < arr[arr.length - 1]) return 0;
@@ -44,26 +40,38 @@ function findRotationCount(arr) {
 }
 
 
+// With rotation count, I should know where to start, and I can shorten array. Does it matter?
+
+//[6, 7, 8, 9, 1, 2, 3, 4] , 8
+// I know rotation is 4
+// check [3] and [4]. If target larger than [3] AND smaller than [4], doesn't exist in array
+// If target < [3] -> target on left side potentially, else right side
+// check each middle to find number and return index. If not found, return -1
 
 
 function findRotatedIndex(arr, target) {
 
-    let rotationIndex = findRotationCount(arr);
-    let left, right, middle;
+  let rotationIndex = findRotationCount(arr) - 1;
+  let left, right
+  
+  if (arr[0] === target) return 0;
+  if (arr[arr.length - 1] === target) return arr.length - 1;
+  if (arr[rotationIndex] === target) return rotationIndex;
+  if (arr[rotationIndex + 1] === target) return rotationIndex + 1;
+  if (arr[rotationIndex - 1] === target) return rotationIndex - 1;
+  if ((target > arr[rotationIndex] && target > arr[arr.length - 1]) || (target < arr[rotationIndex+1] && target < arr[0])) return -1;
 
-    if (arr[rotationIndex - 1] === target) {
-        return rotationIndex - 1;
-    } else if (arr[rotationIndex - 1] < target) {
-        left = rotationIndex;
+   if (rotationIndex === 0){
+       left = 0;
+       right = arr.length - 1;
+   } else if (arr[0] < target  && target < arr[rotationIndex]) { // Working on left
+        left = 1;
+        right = rotationIndex -1;
+   } else if (arr[arr.length - 1] > target && target > arr[rotationIndex+1]) { // Working on right
+        left = rotationIndex + 1;
         right = arr.length - 1;
-    } else if (arr[rotationIndex - 1] > target) {
-        left = 0;
-        right = rotationIndex - 2;
-    } else {
-        left = 0;
-        right = arr.length - 1;
-    }
-    console.log(left, right, arr[left], arr[right], target);
+   } 
+   
     while (left <= right) {
       let middle = Math.floor((left + right) / 2);
 
@@ -76,8 +84,8 @@ function findRotatedIndex(arr, target) {
       }
     }
 
-    
-    return arr[left] < arr[right] ? left : right;
+    if (!left && !right) return -1; // Not found in array
+    return arr[left] < arr[right] ? left : right - 1;
 
 
  }
