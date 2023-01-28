87cb12c44d269c7c559d6242f702257340790c73
Alex Howard <alex-m-howard@pm.me>
2023-01-28 16:35:02 -0500

    finished revString() recursion

--- Section 5/47 - Data Structures and Algorithms/47.6 - Recursion/exercise/recursion.js
+++ Section 5/47 - Data Structures and Algorithms/47.6 - Recursion/exercise/recursion.js
@@ -63,7 +63,11 @@ function findIndex(arr, val) {
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str) {
+  if (!str) {
+    return "";
+  }
 
+  return str[str.length - 1] + revString(str.slice(0, str.length - 1));
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
