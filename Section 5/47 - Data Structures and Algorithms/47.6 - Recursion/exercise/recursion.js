79447ad238737d40477dcbaea8549006bc281b1f
Alex Howard <alex-m-howard@pm.me>
2023-01-27 20:15:36 -0500

    Finished recursion product, longest, everyOther, and isPlaindrome

--- Section 5/47 - Data Structures and Algorithms/47.6 - Recursion/exercise/recursion.js
+++ Section 5/47 - Data Structures and Algorithms/47.6 - Recursion/exercise/recursion.js
@@ -1,25 +1,44 @@
 /** product: calculate the product of an array of numbers. */
 
 function product(nums) {
-
+  if (!nums.length) {
+    return 1;
+  } else {
+    return nums.pop() * product(nums);
+  }
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-
+  if (!words.length) {
+    return words.length;
+  } else {
+    let word = words.pop();
+    return Math.max(word.length, longest(words));
+  }
 }
 
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str) {
-
+  if (!str) {
+    return "";
+  } else {
+    return str.slice(0, 1) + everyOther(str.slice(2));
+  }
 }
+
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
-
+  console.log(str);
+  if (!str) {
+    return "";
+  } else {
+    return str.slice(0) === isPalindrome(str.slice(1).slice(str.length - 1));
+  }
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
