f0982fd845d903a80197d172f8e65cc4482603c8
Alex Howard <alex-m-howard@pm.me>
2023-02-01 18:59:57 -0500

    Finished tree algorithm methods

--- Section 5/47 - Data Structures and Algorithms/47.8 - Trees/exercise/tree.js
+++ Section 5/47 - Data Structures and Algorithms/47.8 - Trees/exercise/tree.js
@@ -15,20 +15,75 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let sum = 0;
+
+    if (!this.root) {
+      return sum;
+    }
+
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      sum += current.val;
+
+      for (let child of current.children) {
+        toVisitStack.push(child)
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let total = 0;
 
+    if (!this.root) {
+      return total;
+    }
+
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      if (current.val % 2 === 0) {
+        total++;
+      };
+
+      for (let child of current.children) {
+        toVisitStack.push(child);
+      }
+    }
+    return total;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let total = 0;
 
+    if (!this.root) {
+      return total;
+    }
+
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      if (current.val > lowerBound) {
+        total++;
+      }
+
+      for (let child of current.children) {
+        toVisitStack.push(child);
+      }
+    }
+    return total;
   }
 }
 
