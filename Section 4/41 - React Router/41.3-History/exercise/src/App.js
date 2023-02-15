bc0818db3bc2a4fbae22c54c7b8cfed83d7867fb
Alex Howard <alex-m-howard@pm.me>
2023-02-15 07:39:44 -0500

    Added 41.3 exercise

---
+++ Section 4/41 - React Router/41.3-History/exercise/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import JokeList from "./JokeList";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <JokeList />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <JokeList />
+//     </div>
+//   );
+// }
+
+// export default App;
